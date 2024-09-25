d00581c1f7552616abc68e5618351e0ce0f24292
omega552003 <omega552003@gmail.com>
2024-09-25 10:12:48 -0400

    Added argument option handling to allow for prefilled filename.

--- floppycopy.sh
+++ floppycopy.sh
@@ -1,10 +1,53 @@
 #! /bin/bash
 
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
+   echo "floppycopy creates a dump, extracts and compresses a floppy disk."
+   echo
+   echo "Syntax: floppycopy.sh [-f|h]"
+   echo "options:"
+   #echo "g     Print the GPL license notification."
+   echo "f     output filename."
+   echo "h     Print this Help."
+   #echo "v     Verbose mode."
+   #echo "V     Print software version and exit."
+   echo
+}
+
+############################################################
+############################################################
+# Main program                                             #
+############################################################
+############################################################
+
 # Set variables
 ROOTDIR=$(dirname $0)
 
+############################################################
+# Process the input options. Add options as needed.        #
+############################################################
+# Get the options
+while getopts ":hn:" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+      f) # Enter a name
+         FILENAME=$OPTARG;;
+     \?) # Invalid option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
 # Get file name from user input
-read -p "Enter Filename: " FILENAME
+if [ ! $FILENAME ]; then
+    read -p "Enter Filename: " FILENAME
+fi
 
 # Set up directories
 DESTINATION="$ROOTDIR/$FILENAME"
